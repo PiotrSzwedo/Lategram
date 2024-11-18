b818b03d26421a085a346d5da4371d1142bd12cc
PiotrSzwedo <piotrszwedo2007@gmail.com>
2024-11-18 20:32:35 +0100

    Updated database migration

--- database/migrations/2024_11_16_230816_create_comments_table.php
+++ database/migrations/2024_11_16_230816_create_comments_table.php
@@ -18,8 +18,6 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->longText('body')->nullable();
-            $table->foreignId('comment_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->boolean("is_under_comment")->default(0);
             $table->timestamps();
         });
     }
--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -40,25 +40,25 @@
 </div>
 
 <script>
-function addComment(form, action) {
-    if (!form || !action) {
-        console.error("Form or action URL is missing.");
-        return;
-    }
-
-    const formData = new FormData(form);
-
-    fetch(action, {
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => {
-        if (response.ok){
-            form.reset();
+    function addComment(form, action) {
+        if (!form || !action) {
+            console.error("Form or action URL is missing.");
+            return;
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+
+        const formData = new FormData(form);
+
+        fetch(action, {
+                method: 'POST',
+                body: formData,
+            })
+            .then(response => {
+                if (response.ok) {
+                    form.reset();
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
 </script>
