d1d4afb61eace1f1afd5c02d77b402c23adc5745
PiotrSzwedo <piotrszwedo2007@gmail.com>
2024-11-23 19:34:12 +0100

    Updated CSS

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -9,32 +9,39 @@
             <img class="pt-2 w-100" src="/storage/{{ $post->image }}" alt="">
         </div>
 
-        <div class="position-relative col-5 border-start">
+        <div class="position-relative col-5 border-start" style="overflow: hidden;">
             <div>
                 <h1 class="mb-0">{{ $post->user->name }}</h1>
             </div>
 
-            <div>
-                <p>{{ $post->body }}</p>
-            </div>
-
-            <div class="comment">
-                @foreach ($post->comments as $comment)
-                @include("components.comment", ["comment" => $comment])
-                @endforeach
-            </div>
-
-            <form id="add-comment-{{ $post->id }}" action="javascript: addComment(document.getElementById('add-comment-{{ $post->id }}'), '{{ route("add_comment") }}')" method="post">
-                @csrf
-                <input type="hidden" name="post_id" value="{{$post->id}}">
-                <div class="row">
-                    <input type="text" class="form-control-file" id="body" name="body"></input>
-
-                    @if ($errors->has('body'))
-                    <strong>{{ $errors->first('body') }}</strong>
-                    @endif
+            <div class="d-flex flex-column h-100">
+                <div class="h-25">
+                    <p class="overflow-auto">{{ $post->body }}</p>
                 </div>
-            </form>
+
+                <div class="comment" style="height: 63% !important;">
+                    @foreach ($post->comments as $comment)
+                    @include("components.comment", ["comment" => $comment])
+                    @endforeach
+                </div>
+
+                <div class="comment-filed">
+                    <form
+                        id="add-comment-{{ $post->id }}"
+                        action="javascript: addComment(document.getElementById('add-comment-{{ $post->id }}'), '{{ route("add_comment") }}')"
+                        method="post">
+                        @csrf
+                        <input type="hidden" name="post_id" value="{{$post->id}}">
+                        <div>
+                            <input type="text" class="form-control-file w-100 p-0" id="body" name="body"></input>
+
+                            @if ($errors->has('body'))
+                            <strong>{{ $errors->first('body') }}</strong>
+                            @endif
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
